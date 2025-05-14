906d4d5647c264a433ef3e8b6142812e7b421f64
Vishwa Warnakulasuriya <vishwanaveen.personal@gmail.com>
2025-05-14 19:04:21 +0530

    added a new question

--- Practice Questions for a Internship/src/main/java/networkflow/Main.java
+++ Practice Questions for a Internship/src/main/java/networkflow/Main.java
@@ -2,9 +2,10 @@ package networkflow;
 
 public class Main {
     public static void main(String[] args) {
-        MoveSpaces rv = new MoveSpaces();
-        char[] s = {'h', 'e', ' ', 'l', 'l', ' ', 'o'};
-        rv.move(s);
+        RemoveCharactors rv = new RemoveCharactors();
+        char[] s = {'a', 'b', 'x', 'c', 'x', 'd'};
+        char ch = 'x';
+        rv.remove(s,ch);
         System.out.println(java.util.Arrays.toString(s)); // Output: [l, e, o, t, c, e, d, e]
     }
 }
