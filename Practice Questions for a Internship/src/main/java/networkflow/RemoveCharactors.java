2a1118ffcb1b2428c802d59cb5209db096943bdd
Vishwa Warnakulasuriya <vishwanaveen.personal@gmail.com>
2025-05-14 19:04:41 +0530

    Update removecharactors

--- Practice Questions for a Internship/src/main/java/networkflow/RemoveCharactors.java
+++ Practice Questions for a Internship/src/main/java/networkflow/RemoveCharactors.java
@@ -1,4 +1,19 @@
 package networkflow;
 
 public class RemoveCharactors {
+    public void remove(char[] s, char ch) {
+        int length = s.length;
+        int write = 0;
+
+        for (int i = 0; i<length; i++){
+            if(s[i] != ch){
+                 s[write] = s[i];
+                 write++;
+            }
+        }
+        while (write<length){
+            s[write] = ' ';
+            write++;
+        }
+    }
 }
