35023c9ebd2380601c5935a75a226cf82d2d6925
Vishwa Warnakulasuriya <vishwanaveen.personal@gmail.com>
2025-05-14 22:53:15 +0530

    update move zeros with the solution

--- Practice Questions for a Internship/src/main/java/networkflow/MoveZeros.java
+++ Practice Questions for a Internship/src/main/java/networkflow/MoveZeros.java
@@ -1,4 +1,18 @@
 package networkflow;
 
 public class MoveZeros {
+    public void move(int[]nums){
+        int write = 0;
+
+        for (int i = 0; i<nums.length; i++){
+            if(nums[i] != 0){
+                nums[write] = nums[i];
+                write++;
+            }
+        }
+        while (write<nums.length){
+            nums[write] = 0;
+            write++;
+        }
+    }
 }
