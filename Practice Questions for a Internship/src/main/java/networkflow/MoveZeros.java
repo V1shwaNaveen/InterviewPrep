3e8e4ecdd501fd7b189f0c7f9479715dbce14166
Vishwa Warnakulasuriya <vishwanaveen.personal@gmail.com>
2025-05-17 22:37:31 +0530

    added movezeros

--- Practice Questions for a Internship/src/main/java/networkflow/MoveZeros.java
+++ Practice Questions for a Internship/src/main/java/networkflow/MoveZeros.java
@@ -3,7 +3,7 @@ package networkflow;
 public class MoveZeros {
     public void move(int[]nums){
         int write = 0;
-
+        int num = 0;
         for (int i = 0; i<nums.length; i++){
             if(nums[i] != 0){
                 nums[write] = nums[i];
